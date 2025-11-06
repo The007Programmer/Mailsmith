469726e5471e5525514ef010b4bb497ecc1faff4
The007Programmer <aahils2019@gmail.com>
2025-11-06 15:25:37 -0600

    UNST: idk what i changed here either

--- openai_api.py
+++ openai_api.py
@@ -1,19 +1,16 @@
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
 import json
 
-# Open json file with openai api key.
-with open("secrets/openai_key.json", "r") as keyfile:
-    data = json.load(keyfile)
+load_dotenv()
 
 # Initialize the OpenAI client with the API key
-client = OpenAI(api_key=data["OPENAI_API_KEY"])
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
+response = client.responses.create(
+    model="gpt-5-nano",
+    input="Write a short bedtime story about a unicorn."
 )
 
-print(completion.choices[0].message)
+print(response.output_text)
